f33cbc97f0720d108e3bf904316a3dd18872a84d
tuna bilgili <zeng408408@gmail.com>
2022-06-23 22:24:58 +0300

    added bootstrap to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import LeaderBoardTable from "../components/LeaderBoardTable";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 
 function HomePage() {
-    return <LeaderBoardTable region={"eu"} />;
+    return (
+        <Container>
+            <Row>
+                <Col><LeaderBoardTable region={"eu"}/></Col>
+                <Col><LeaderBoardTable region={"na"}/></Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default HomePage;
