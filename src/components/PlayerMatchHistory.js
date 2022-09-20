a3ea58131b1c05db4ed1b74ed13cb1fd50f83a2b
tunabilgili <btunabilgili@gmail.com>
2022-09-20 23:39:18 +0300

    match history added

--- src/components/PlayerMatchHistory.js
+++ src/components/PlayerMatchHistory.js
@@ -55,33 +55,33 @@ function PlayerMatchHistory(props) {
             <div className={`d-flex player-match-container ${data.team.has_won ? "match-won" : "match-lost"}`} style={{ height: "80px" }} key={index}>
               <div className="d-flex w-100 justify-content-between m-2 p-2">
                 <div className="d-flex flex-column justify-content-center align-items-center">
-                  <div className="match-history-label" style={{ color: data.team.has_won ? "rgb(127 195 255)" : "rgb(255 70 85)" }}>{data.team.has_won ? "Victory" : "Defeat"}</div>
+                  <div className="match-history-label" style={{ color: data.team.has_won ? "rgb(127 195 255)" : "rgb(255 70 85)", fontWeight: "bold" }}>{data.team.has_won ? "Victory" : "Defeat"}</div>
                   <div className="match-history-label">Competitive</div>
                   <div className="match-history-label">{new Date(data.metadata.game_start_patched).toLocaleTimeString("en-US")}</div>
                 </div>
                 <div className="d-flex flex-column justify-content-center align-items-center"><img src={data.player.assets.agent.small} className={`player-portrait ${data.team.has_won ? "player-portrait-won" : "player-portrait-lost"}`} alt="player avatar" /></div>
                 <div className="d-flex flex-column justify-content-center align-items-center">
-                  <div>{data.team.rounds_won} - {data.team.rounds_lost}</div>
+                  <div style={{ fontWeight: "bold" }}><span style={{ color: "#7fc3ff" }}>{data.team.rounds_won}&nbsp;-&nbsp;</span><span style={{ color: "#ff4655" }}>{data.team.rounds_lost}</span></div>
                   <div className="match-history-label" style={{ fontWeight: "bold" }}>{data.metadata.map}</div>
                 </div>
                 <div className="d-flex flex-column justify-content-center align-items-center">
-                  <div>{parseInt(data.player.stats.score / data.metadata.rounds_played)}</div>
+                  <div className="bold">{parseInt(data.player.stats.score / data.metadata.rounds_played)}</div>
                   <div className="match-history-label">Combat Score</div>
                 </div>
                 <div className="d-none d-lg-flex flex-column justify-content-center align-items-center">
-                  <div>{data.player.stats.kills + "/" + data.player.stats.deaths + "/" + data.player.stats.assists}</div>
+                  <div className="bold">{data.player.stats.kills + "/" + data.player.stats.deaths + "/" + data.player.stats.assists}</div>
                   <div className="match-history-label">KDA</div>
                 </div>
                 <div className="d-none d-md-flex flex-column justify-content-center align-items-center">
-                  <div>{Math.round((data.player.stats.kills / data.player.stats.deaths) * 100) / 100}</div>
+                  <div className="bold">{Math.round((data.player.stats.kills / data.player.stats.deaths) * 100) / 100}</div>
                   <div className="match-history-label">KD</div>
                 </div>
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <div>{Math.round((data.player.damage_made / data.metadata.rounds_played) * 100) / 100}</div>
+                <div className="d-none d-sm-flex flex-column justify-content-center align-items-center">
+                  <div className="bold">{Math.round((data.player.damage_made / data.metadata.rounds_played) * 100) / 100}</div>
                   <div className="match-history-label">Avg.Dmg</div>
                 </div>
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <div>{((data.player.stats.headshots / (data.player.stats.headshots + data.player.stats.bodyshots + data.player.stats.legshots)) * 100).toFixed(2)}%</div>
+                <div className="d-none d-sm-flex flex-column justify-content-center align-items-center">
+                  <div className="bold">{((data.player.stats.headshots / (data.player.stats.headshots + data.player.stats.bodyshots + data.player.stats.legshots)) * 100).toFixed(2)}%</div>
                   <div className="match-history-label">HS%</div>
                 </div>
               </div>
